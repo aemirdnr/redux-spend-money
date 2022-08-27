75ed74b35fdb813dd00c1b328dcbe945bf08ea24
aemirdnr <aemirdnr@gmail.com>
2022-08-27 06:00:23 +0300

    Remove images & Path fixes from JSON file

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import "../style.sass";
 function Header() {
   return (
     <div className="header_box mt-4">
-      <img src="src/images/buffett.jpg" />
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Warren_Buffett_at_the_2015_SelectUSA_Investment_Summit.jpg/1200px-Warren_Buffett_at_the_2015_SelectUSA_Investment_Summit.jpg" />
       <h1>Spend Warren Buffett's Money</h1>
     </div>
   );
